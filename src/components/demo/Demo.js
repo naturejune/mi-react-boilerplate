a3c52e5f491e9890623847de61af0e9672c80382
jiangzg <jiangzg@fenbi.com>
2016-11-28 00:06:44 +0800

    init

---
+++ src/components/demo/Demo.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import style from './Demo.module.css';
+
+const Demo = () => <p className={style.root}>Demo</p>;
+
+export default Demo;
---
+++ webpack.config.prod.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: './src/index',
+  output: {
+    filename: 'index.js',
+    path: __dirname + '/lib',
+    library: 'mi',
+    libraryTarget: 'umd'
+  },
+  externals: {
+    react: {
+      root: 'React',
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react'
+    }
+  },
+  module: {
+    loaders: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loaders: ['babel']
+    }, {
+      test: /\.module.css$/,
+      loader: 'style-loader!css-loader?modules'
+    }]
+  }
+}
